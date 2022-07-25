625fa5dc16cf565a80c1ae50616a342f58f0dbef
gauravc574 <gauravchawre580@gmail.com>
2022-07-25 23:19:32 +0530

    fibonacci done

--- Recursion/fibonacci_series.py
+++ Recursion/fibonacci_series.py
@@ -1,7 +1,9 @@
 def fibonacci_series(n):
-    if n == 1:
+    if n == 1 or n == 2:
         return 1
-    fibonacci_series(n -1) + fibonacci_series(n-2)
-    print(n )
-fibonacci_series(5)
+    
+    return fibonacci_series(n-1) + fibonacci_series(n-2)
+    print(n) 
+    
+print(fibonacci_series(4))
 
